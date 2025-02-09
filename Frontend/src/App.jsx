377712b69f58a8b7d8b016f9a0e9360494ade1d7
Kirri Tharun <tharunprajitha2023@gmail.com>
2025-02-09 23:52:36 +0530

    day-1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import HomePage from './landing_page/home/HomePage'
 
 function App() {
   return (
     <>
+      <HomePage/>
     </>
   )
 }
---
+++ Frontend/src/landing_page/home/Invest.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Invest() {
+    return (
+        <>
+        <h1>Invest Section</h1>
+        </>
+    );
+}
+export default Invest;
