c6d3e05e4538b0b9c7c79f10694419b70e52c497
Kirri Tharun <tharunprajitha2023@gmail.com>
2025-02-13 15:53:51 +0530

    support page updated

--- Frontend/src/landing_page/support/Hero.jsx
+++ Frontend/src/landing_page/support/Hero.jsx
@@ -2,30 +2,7 @@ import './Hero.css'
 function Hero() {
     return (
         <>
-            {/* <div style={{ backgroundColor: "#387ed1" }}>
-                <div className="container p-5">
-                    <div className="row">
-                        <div className="col p-3">
-                            <h3>Support Portal</h3>
-                            <p>Search for an answer or browse help topics to create a ticket</p>
-                            <input id="search-text" type="text" class="col-6" placeholder="Eg: how do i activate F&amp;O, why is my order getting rejected ..." ></input><br />
-                            <a href="">Track account opening</a>
-                            <a href="">Track segment activation</a>
-                            <a href="">Intraday margins</a> <br />
-                            <a href="">Kite user manual</a>
-                        </div>
-                        <div className="col">
-                            <a href="">Track tickets</a>
-                            <div>
-                                <h3>Featured</h3>
-                                <p>1. <a href="" >Surveillance measure on scrips - February 2025</a></p>
-                                <p>2. <a href="">Latest Intraday leverages and Square-off timings</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div > */}
-            <div style={{ backgroundColor: "#387ed1",paddingBottom:"2rem"}}>
+            <div style={{ backgroundColor: "#387ed1", paddingBottom: "2rem" }}>
                 <div className="container p-5" style={{ color: "white" }}>
                     <div className="mt-3 mb-3 portal" style={{ display: "flex", justifyContent: "space-between" }}>
                         <p className='fs-4'>Support Portal</p>
@@ -35,20 +12,22 @@ function Hero() {
                     <div className="row portal">
                         <div className="col-7"><p className="fs-4">
                             Search for an answer or browse help topics to create a ticket</p>
-                            <input id="search-text" type="text" placeholder="Eg: how do i activate F&amp;O, why is my order getting rejected ..." style={{width:"95%" ,padding:" 0.8rem 1rem" ,borderRadius:"5px",border:"none"}}></input><br />
+                            <input id="search-text" type="text" placeholder="Eg: how do i activate F&amp;O, why is my order getting rejected ..." style={{ width: "95%", padding: " 0.8rem 1rem", borderRadius: "5px", border: "none" }}></input><br />
                             <br />
-                            <a className="" href="" style={{ color: "white" ,textDecoration:"none", borderBottom:"2px solid white", paddingBottom:"2px" , marginRight:"2rem" }}>Track account opening</a>
-                            <a href="" style={{ color: "white" ,textDecoration:"none", borderBottom:"2px solid white", paddingBottom:"2px" , marginRight:"2rem" }}>Track segment activation</a>
-                            <a href="" style={{ color: "white" ,textDecoration:"none", borderBottom:"2px solid white", paddingBottom:"2px" , marginRight:"2rem"}}>Intraday margins</a>
+                            <a href="" style={{ color: "white", textDecoration: "none", borderBottom: "1px solid white", paddingBottom: "2px", marginRight: "2rem" }}>Track account opening</a>
+                            <a href="" style={{ color: "white", textDecoration: "none", borderBottom: "1px solid white", paddingBottom: "2px", marginRight: "2rem" }}>Track segment activation</a>
+                            <a href="" style={{ color: "white", textDecoration: "none", borderBottom: "1px solid white", paddingBottom: "2px", marginRight: "2rem" }}>Intraday margins</a>
                             <br />
                             <br />
-                            <a href="" style={{ color: "white" ,textDecoration:"none", borderBottom:"2px solid white", paddingBottom:"2px" , marginRight:"1rem"}}>Kite user manual</a><br />
+                            <a href="" style={{ color: "white", textDecoration: "none", borderBottom: "1px solid white", paddingBottom: "2px", marginRight: "1rem" }}>Kite user manual</a><br />
                         </div>
-                    
-                        <div className="col-5 mt-5" style={{ color: "white",paddingLeft:"4rem"}}>
-                            <h3>Featured</h3>
-                            <p style={{paddingLeft:"1rem"}}>1. <a href="" style={{ color: "white"}}>Surveillance measure on scrips - February 2025</a></p>
-                            <p style={{paddingLeft:"1rem"}}>2. <a href="" style={{ color: "white"}}>Latest Intraday leverages and Square-off timings</a></p>
+
+                        <div className="col-5 mt-3" style={{ color: "white", paddingLeft: "3.5rem" }}>
+                            <h3 className='mb-4 fs-5'>Featured</h3>
+                            <ol>
+                                <li style={{paddingBottom:"1rem"}}><a href="" style={{ color: "white", fontSize: "1.05rem"}}>Adjustment of F&O contracts of NATIONALUM due to dividend</a></li>
+                                <li><a href="" style={{ color: "white", fontSize: "1.05rem" }}> Surveillance measure on scrips - February 2025</a></li>
+                            </ol>
                         </div>
                     </div>
                 </div>
--- Frontend/src/landing_page/support/Ticket.jsx
+++ Frontend/src/landing_page/support/Ticket.jsx
@@ -1,7 +1,51 @@
 function Ticket() {
     return (
         <>
-            <h1>Ticket Section</h1>
+            <div style={{ paddingBottom: "2rem" }}>
+                <div className="container p-5">
+                    <div className="mt-3 mb-3 portal">
+                        <p className='fs-4 text-muted'>To create a ticket, select a relevant topic</p>
+                        <div className="row">
+                            <div className="col">
+                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "black" }}><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</a></p>
+                                <p style={{ paddingLeft: "1.3rem" }}>
+                                    <img src="" alt="" />
+                                    <a href="" style={{ lineHeight: "2" }}>Getting started</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Online</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Offline</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Charges</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Company, Partnership and HUF</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Non Resident Indian (NRI)</a>
+                                </p>
+                            </div>
+                            <div className="col">
+                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "black" }}><i class="fa-regular fa-user fs-5"></i>  Your Zerodha Account</a></p>
+                                <p style={{ paddingLeft: "1.3rem" }}>
+                                    <img src="" alt="" />
+                                    <a href="" style={{ lineHeight: "2" }}>Getting started</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Online</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Offline</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Charges</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Company, Partnership and HUF</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Non Resident Indian (NRI)</a>
+                                </p>
+                            </div>
+                            <div className="col">
+                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "black" }}><i class="fa fa-plus-circle" aria-hidden="true"></i>  Trading and Markets</a></p>
+                                <p style={{ paddingLeft: "1.3rem" }}>
+                                    <img src="" alt="" />
+                                    <a href="" style={{ lineHeight: "2" }}>Getting started</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Online</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Offline</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Charges</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Company, Partnership and HUF</a><br />
+                                    <a href="" style={{ lineHeight: "2" }}>Non Resident Indian (NRI)</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
