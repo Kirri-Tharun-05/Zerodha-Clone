6dbbd6076682e0e24059cd5a8f5f3868787efadf
Kirri Tharun <tharunprajitha2023@gmail.com>
2025-02-14 11:50:11 +0530

    updated support page

--- Frontend/src/landing_page/Footer.jsx
+++ Frontend/src/landing_page/Footer.jsx
@@ -11,34 +11,34 @@ function Footer() {
                             <p className="mt-4" style={{ fontSize: "0.9rem" }}>© 2010 - 2025, Zerodha Broking Ltd.<br />
                                 All rights reserved.</p>
                         </div>
-                        <div className="col " style={{ textDecoration: "none" }}>
+                        <div className="col" style={{ textDecoration: "none" }}>
                             <p>Company</p>
-                            <a href="">About</a><br />
-                            <a href="">Products</a><br />
-                            <a href="">Pricing</a><br />
-                            <a href="">Referral programme</a><br />
-                            <a href="">Careers</a><br />
-                            <a href="">Zerodha.tech</a><br />
-                            <a href="">Open source</a><br />
-                            <a href="">Press & media</a><br />
-                            <a href="">Zerodha Cares (CSR)</a><br />
+                            <a href="" className="text-muted">About</a><br />
+                            <a href="" className="text-muted">Products</a><br />
+                            <a href="" className="text-muted">Pricing</a><br />
+                            <a href="" className="text-muted">Referral programme</a><br />
+                            <a href="" className="text-muted">Careers</a><br />
+                            <a href="" className="text-muted">Zerodha.tech</a><br />
+                            <a href="" className="text-muted">Open source</a><br />
+                            <a href="" className="text-muted">Press & media</a><br />
+                            <a href="" className="text-muted">Zerodha Cares (CSR)</a><br />
                         </div>
                         <div className="col ">
                             <p>Support</p>
-                            <a href="">Contact us</a><br />
-                            <a href="">Support portal</a>  <br /> .
-                            <a href="">Z-Connect blog</a><br />
-                            <a href="">List of charges</a><br />
-                            <a href="">Downloads & resources</a><br />
-                            <a href="">Videos</a><br />
-                            <a href="">Market overview</a><br />
-                            <a href="">How to file a complaint?</a><br />
-                            <a href="">Status of your complaints</a><br />
+                            <a href="" className="text-muted">Contact us</a><br />
+                            <a href="" className="text-muted">Support portal</a>  <br /> .
+                            <a href="" className="text-muted">Z-Connect blog</a><br />
+                            <a href="" className="text-muted">List of charges</a><br />
+                            <a href="" className="text-muted">Downloads & resources</a><br />
+                            <a href="" className="text-muted">Videos</a><br />
+                            <a href="" className="text-muted">Market overview</a><br />
+                            <a href="" className="text-muted">How to file a complaint?</a><br />
+                            <a href="" className="text-muted">Status of your complaints</a><br />
                         </div>
                         <div className="col ">
                             <p >Account</p>
-                            <a href="">Open an account</a><br />
-                            <a href="">Fund transfer</a><br />
+                            <a href="" className="text-muted">Open an account</a><br />
+                            <a href="" className="text-muted">Fund transfer</a><br />
                         </div>
                     </div>
                     <div>
@@ -58,15 +58,15 @@ function Footer() {
                                 "Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.
                             </p>
                         </div>
-                        <div className="footer-links mb-4"  style={{ fontSize: "1rem", padding: "0 7rem",}}>
-                                <a href="">NSE</a>
-                                <a href="">BSE</a>
-                                <a href="">Terms & conditions</a>
-                                <a href="">Policies & procedures</a>
-                                <a href="">Privacy policy</a>
-                                <a href="">Disclosure</a>
-                                <a href="">For investor's attention</a>
-                                <a href="">Investor charter</a>
+                        <div className="footer-links mb-4" style={{ fontSize: "1rem", padding: "0 7rem", }}>
+                            <a href="">NSE</a>
+                            <a href="">BSE</a>
+                            <a href="">Terms & conditions</a>
+                            <a href="">Policies & procedures</a>
+                            <a href="">Privacy policy</a>
+                            <a href="">Disclosure</a>
+                            <a href="">For investor's attention</a>
+                            <a href="">Investor charter</a>
                         </div>
                     </div>
                 </div>
--- Frontend/src/landing_page/support/Ticket.jsx
+++ Frontend/src/landing_page/support/Ticket.jsx
@@ -5,41 +5,75 @@ function Ticket() {
                 <div className="container p-5">
                     <div className="mt-3 mb-3 portal">
                         <p className='fs-4 text-muted'>To create a ticket, select a relevant topic</p>
-                        <div className="row">
-                            <div className="col">
-                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "black" }}><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</a></p>
+                        <div className="row mt-5">
+                            <div className="col" style={{ color: "#387ed1" }}>
+                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "#222" }}><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</a></p>
+                                <p style={{ paddingLeft: "1.3rem" }}>
+                                    <a href="" style={{ lineHeight: "2", color: "#387ed1" }}>Getting started</a><br />
+                                    <a href="" style={{ lineHeight: "2", color: "#387ed1" }}>Online</a><br />
+                                    <a href="" style={{ lineHeight: "2", color: "#387ed1" }}>Offline</a><br />
+                                    <a href="" style={{ lineHeight: "2", color: "#387ed1" }}>Charges</a><br />
+                                    <a href="" style={{ lineHeight: "2", color: "#387ed1" }}>Company, Partnership and HUF</a><br />
+                                    <a href="" style={{ lineHeight: "2", color: "#387ed1" }}>Non Resident Indian (NRI)</a>
+                                </p>
+                            </div>
+                            <div className="col" style={{ color: "#387ed1" }}>
+                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "#222" }}><i class="fa-regular fa-user fs-5"></i>  Your Zerodha Account</a></p>
                                 <p style={{ paddingLeft: "1.3rem" }}>
                                     <img src="" alt="" />
-                                    <a href="" style={{ lineHeight: "2" }}>Getting started</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Online</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Offline</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Charges</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Company, Partnership and HUF</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Non Resident Indian (NRI)</a>
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Login credentials</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Your Profile</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Account modification and segment addition</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>CMR & DP ID</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Nomination</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Transfer and conversion of shares</a>
+                                </p>
+                            </div>
+                            <div className="col" style={{ color: "#387ed1" }}>
+                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "#222" }}> <img src="images/stock-market.png" style={{width:"1.3rem", paddingBottom:"8px"}}/>  Trading and Markets</a></p>
+                                <p style={{ paddingLeft: "1.3rem" }}>
+                                    <img src="" alt="" />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Trading FAQs</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Kite</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Margins</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Product and order types</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Corporate actions</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Kite features</a>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="row mt-5">
+                            <div className="col">
+                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "#222" }}><img src="images/credit-card.png" style={{width:"1.3rem", paddingBottom:"2px"}}/> Funds</a></p>
+                                <p style={{ paddingLeft: "1.3rem" }}>
+                                    <img src="" alt="" />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Fund withdrawal</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Adding funds</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Adding bank accounts</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>eMandates</a><br />
                                 </p>
                             </div>
                             <div className="col">
-                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "black" }}><i class="fa-regular fa-user fs-5"></i>  Your Zerodha Account</a></p>
+                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "#222" }}><img src="images/button.png" style={{width:"1.1rem", paddingBottom:"8px"}}/> Console</a></p>
                                 <p style={{ paddingLeft: "1.3rem" }}>
                                     <img src="" alt="" />
-                                    <a href="" style={{ lineHeight: "2" }}>Getting started</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Online</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Offline</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Charges</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Company, Partnership and HUF</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Non Resident Indian (NRI)</a>
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>IPO</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Portfolio</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Funds statement</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Profile</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Reports</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Referral program</a>
                                 </p>
                             </div>
                             <div className="col">
-                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "black" }}><i class="fa fa-plus-circle" aria-hidden="true"></i>  Trading and Markets</a></p>
+                                <p className="fs-5 mb-4"> <a href="" style={{ textDecoration: "none", color: "#222" }}><img src="images/rec.png" style={{width:"1.2rem", paddingBottom:"3px"}}/> Coin</a></p>
                                 <p style={{ paddingLeft: "1.3rem" }}>
                                     <img src="" alt="" />
-                                    <a href="" style={{ lineHeight: "2" }}>Getting started</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Online</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Offline</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Charges</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Company, Partnership and HUF</a><br />
-                                    <a href="" style={{ lineHeight: "2" }}>Non Resident Indian (NRI)</a>
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Understanding mutual funds and Coin</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Coin app</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Coin web</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Product and order types</a><br />
+                                    <a href="" style={{ lineHeight: "2",color: "#387ed1" }}>Transactions and reports</a><br />
                                 </p>
                             </div>
                         </div>
