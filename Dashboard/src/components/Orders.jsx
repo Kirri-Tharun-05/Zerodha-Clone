f0d5a99c8cc0f11f8b7256bc00f19dfb9876bb60
Kirri Tharun <tharunprajitha2023@gmail.com>
2025-02-19 08:37:10 +0530

    updated Orders Route

--- Dashboard/src/components/Orders.jsx
+++ Dashboard/src/components/Orders.jsx
@@ -5,17 +5,17 @@ import axios from 'axios'
 function Orders() {
   const [allOrders, setallOrders] = useState([]);
   useEffect(() => {
-    // axios.get(`${import.meta.env.BACKEND_URL}/allOrders`).then((res)=>{
-    //   setallOrders(res.data)
-    // })
-    axios.get(`${import.meta.env.BACKEND_URL}/allOrders`).then((res) => {
-      console.log("API Response:", res.data);  // Debugging log
-      setallOrders(Array.isArray(res.data) ? res.data : []); // Ensure it's always an array
+    axios.get(`${import.meta.env.BACKEND_URL}/allOrders`).then((res)=>{
+      setallOrders(res.data)
     })
-      .catch((error) => {
-        console.error("Error fetching orders:", error);
-        setallOrders([]); // Set empty array on error to prevent crashes
-      });
+    // axios.get(`${import.meta.env.BACKEND_URL}/allOrders`).then((res) => {
+    //   console.log("API Response:", res.data);  // Debugging log
+    //   setallOrders(Array.isArray(res.data) ? res.data : []); // Ensure it's always an array
+    // })
+    //   .catch((error) => {
+    //     console.error("Error fetching orders:", error);
+    //     setallOrders([]); // Set empty array on error to prevent crashes
+    //   });
   }, []);
   return (
     <div className="orders">
